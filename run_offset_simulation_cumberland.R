47635d717d18f01becf2c0560baa83d8e235755e
isaacpeterson <isaac.p.mail@gmail.com>
2018-09-22 14:41:03 +1000

    change params to match repo

--- run_offset_simulation_cumberland.R
+++ run_offset_simulation_cumberland.R
@@ -7,7 +7,7 @@ user_global_params = initialise_user_global_params()
 user_feature_params = initialise_user_feature_params()
 user_output_params <- initialise_user_output_params()
 
-osim.run(user_global_params, user_simulation_params, user_feature_params, loglevel = 'TRACE')
+osim.run(user_global_params, user_simulation_params, user_feature_params, user_transform_function, loglevel = 'TRACE')
 
 cat('\nFinished osim.run(), starting osim.output() \n')
 
